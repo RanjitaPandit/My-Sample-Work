938992f0cccacfc53fdff064af2701eb490d743d
RanjitaPandit <ranjitapandit@gmail.com>
2019-05-17 12:32:24 +1000

    Sample Nunit Framework

---
+++ MarsFramework/Pages/SignIn.cs
@@ -0,0 +1,64 @@
+﻿using MarsFramework.Global;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+
+namespace MarsFramework.Pages
+{
+    internal class SignIn
+    {
+        private RemoteWebDriver _driver;
+        public SignIn(RemoteWebDriver driver) => _driver = driver;
+
+        
+
+ 
+        #region  Initialize Web Elements 
+        //Finding the Sign Link
+        private IWebElement SignIntab => _driver.FindElementByXPath("//*[@id='home']/div/div/div[1]/div/a");
+
+        // Finding the Email Field
+        private IWebElement Email => _driver.FindElementByXPath("/html/body/div[2]/div/div/div[1]/div/div[1]/input");
+
+        //Finding the Password Field
+        private IWebElement Password => _driver.FindElementByXPath("/html/body/div[2]/div/div/div[1]/div/div[2]/input");
+
+        //Finding the Login Button
+        IWebElement LoginBtn => _driver.FindElementByXPath("/html/body/div[2]/div/div/div[1]/div/div[4]/button");
+
+        #endregion
+        #region Login
+        internal void LoginSteps()
+        {
+            //extent Reports
+            Base.test = Base.extent.StartTest("Login Test");
+
+            //Populate the Excel sheet
+            GlobalDefinitions.ExcelLib.PopulateInCollection(Global.Base.ExcelPath, "SignIn");
+
+            //Navigate to the Url
+            _driver.Navigate().GoToUrl(GlobalDefinitions.ExcelLib.ReadData(2, "Url"));
+
+
+            //Click on Sign In tab
+            SignIntab.Click();
+            //Enter the data in Username textbox
+            Email.SendKeys(GlobalDefinitions.ExcelLib.ReadData(2, "Username"));
+
+            //Enter the password 
+            Password.SendKeys(GlobalDefinitions.ExcelLib.ReadData(2, "Password"));
+
+            ////Click on Login button
+            LoginBtn.Click();
+            if (_driver.WaitForElementDisplayed(By.XPath("//a[contains(text(),'Mars Logo')]"), 60))
+            {
+                Base.test.Log(RelevantCodes.ExtentReports.LogStatus.Pass, "Login Successful");
+            } else
+            {
+                Base.test.Log(RelevantCodes.ExtentReports.LogStatus.Fail, "Login failed");
+            }
+
+        }
+        #endregion
+    }
+}
---
+++ MarsFramework/Pages/SignUp.cs
@@ -0,0 +1,72 @@
+﻿using MarsFramework.Global;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+
+namespace MarsFramework.Pages
+{
+    class SignUp
+    {
+        private RemoteWebDriver _driver;
+        public SignUp(RemoteWebDriver driver) => _driver = driver;
+
+    
+
+        #region  Initialize Web Elements
+        //Finding the Join 
+        private IWebElement Join => _driver.FindElementByXPath("//*[@id='home']/div/div/div[1]/div/button");
+
+        //Identify FirstName Textbox
+        private IWebElement FirstName => _driver.FindElementByXPath("/html/body/div[2]/div/div/div/form/div[1]/input");
+
+        //Identify LastName Textbox
+        private IWebElement LastName => _driver.FindElementByXPath("/html/body/div[2]/div/div/div/form/div[2]/input");
+
+        //Identify Email Textbox
+        private IWebElement Email => _driver.FindElementByXPath("/html/body/div[2]/div/div/div/form/div[3]/input");
+
+        //Identify Password Textbox
+        private IWebElement Password => _driver.FindElementByXPath("/html/body/div[2]/div/div/div/form/div[4]/input");
+
+        //Identify Confirm Password Textbox
+        private IWebElement ConfirmPassword => _driver.FindElementByXPath("/html/body/div[2]/div/div/div/form/div[5]/input");
+
+        //Identify Term and Conditions Checkbox
+        private IWebElement Checkbox => _driver.FindElementByXPath("/html/body/div[2]/div/div/div/form/div[6]/div/div/input");
+
+        //Identify join button
+        private IWebElement JoinBtn => _driver.FindElementByXPath("//*[@id='submit-btn']");
+        #endregion
+        #region SignUp 
+        internal void register()
+        {
+            
+            //Click on Join button
+            Join.Click();
+
+            //Enter FirstName
+            FirstName.SendKeys(GlobalDefinitions.ExcelLib.ReadData(2,"FirstName"));
+
+            //Enter LastName
+            LastName.SendKeys(GlobalDefinitions.ExcelLib.ReadData(2, "LastName"));
+
+            //Enter Email
+            Email.SendKeys(GlobalDefinitions.ExcelLib.ReadData(2, "Email"));
+
+            //Enter Password
+            Password.SendKeys(GlobalDefinitions.ExcelLib.ReadData(2, "Password"));
+
+            //Enter Password again to confirm
+            ConfirmPassword.SendKeys(GlobalDefinitions.ExcelLib.ReadData(2, "ConfirmPswd"));
+
+            //Click on Checkbox
+            Checkbox.Click();
+
+            //Click on join button to Sign Up
+            JoinBtn.Click();
+
+            
+        }
+        #endregion
+    }
+}
